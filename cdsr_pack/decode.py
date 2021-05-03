d63aada80323b87d5157161d4e4b0d8429f60e2b
Rodrigo Mariano <rodmariano13@gmail.com>
2021-05-03 16:38:10 -0300

    fix: fix exceptions

--- cdsr_pack/decode.py
+++ cdsr_pack/decode.py
@@ -1,4 +1,4 @@
-"""decode.py"""
+"""decode.py module"""
 
 from os.path import sep as os_path_sep
 
@@ -31,7 +31,7 @@ def decode_scene_dir(scene_dir):
             # `time` can be: `13_53_00`, `13_53_00_ETC2`, `14_35_23_CB11_SIR18`, etc.
             time = ':'.join(time[0:3])
         else:
-            raise CDSRDecodeException(f'Invalid spplited time: `{time}`.')
+            raise CDSRDecodeException(f'Invalid spplited time: `{scene_dir_second}`.')
 
     elif scene_dir_first.startswith('CBERS2B') or scene_dir_first.startswith('LANDSAT'):
         # examples: CBERS2B_CCD_20070925.145654
@@ -49,13 +49,13 @@ def decode_scene_dir(scene_dir):
 
         if len(time) != 6:
             # example: a time should be something like this: '145654'
-            raise CDSRDecodeException(f'Size of `{time}` date is not 6.')
+            raise CDSRDecodeException(f'Size of `{time}` time is not 6.')
 
         # I build the time string based on the old one (e.g. from '145654' to '14:56:54')
         time = f'{time[0:2]}:{time[2:4]}:{time[4:6]}'
 
     else:
-        raise CDSRDecodeException(f'Scene directory cannot be decoded: `{scene_dir}`.')
+        raise CDSRDecodeException(f'Invalid scene directory: `{scene_dir}`.')
 
     return satellite, sensor, date, time
 
@@ -72,7 +72,7 @@ def decode_path_row_dir(path_row_dir):
         # example: `151_B_141_5_0`
         path, _, row, *_ = splitted_path_row
     else:
-        raise CDSRDecodeException(f'Path/row directory cannot be decoded: `{path_row_dir}`.')
+        raise CDSRDecodeException(f'`{path_row_dir}` path/row directory cannot be decoded.')
 
     return path, row
 
@@ -85,8 +85,8 @@ def decode_geo_processing_dir(geo_processing_dir):
     if geo_processing in ('2', '2B', '3', '4'):
         return geo_processing
 
-    raise CDSRDecodeException('Geo. processing directory cannot be '
-                              f'decoded: `{geo_processing_dir}`.')
+    raise CDSRDecodeException(f'`{geo_processing_dir}` geo. processing '
+                                'directory cannot be decoded.')
 
 
 def decode_asset(asset):
@@ -130,15 +130,11 @@ def decode_path(path):
     if level == 7:
         metadata['radio_processing'] = decode_asset(splitted_path[-1])
 
-    # extract `satellite` and `sensor` data from path
+    # extract metadata
     _, metadata['satellite'], _, scene_dir, path_row_dir, geo_processing_dir, *_ = splitted_path
-
-    try:
-        _, metadata['sensor'], *_ = decode_scene_dir(scene_dir)
-        metadata['path'], metadata['row'] = decode_path_row_dir(path_row_dir)
-        metadata['geo_processing'] = decode_geo_processing_dir(geo_processing_dir)
-    except CDSRDecodeException as error:
-        raise CDSRDecodeException(f'Unable to decode path: `{path}`.') from error
+    _, metadata['sensor'], *_ = decode_scene_dir(scene_dir)
+    metadata['path'], metadata['row'] = decode_path_row_dir(path_row_dir)
+    metadata['geo_processing'] = decode_geo_processing_dir(geo_processing_dir)
 
     return metadata
 
