64c98a487bdba340128cb57029fd860ed498c8d7
Rodrigo Mariano <rodmariano13@gmail.com>
2021-05-04 11:22:31 -0300

    refactor: refactor decode.py module

--- cdsr_pack/decoder.py
+++ cdsr_pack/decoder.py
@@ -1,10 +1,10 @@
-"""decode.py module"""
+"""decoder.py module."""
 
 from os.path import sep as os_path_sep
 
 
-class CDSRDecodeException(Exception):
-    """CDSRDecodeException"""
+class CDSRDecoderException(Exception):
+    """CDSRDecoderException."""
 
 
 def decode_scene_dir(scene_dir):
@@ -31,7 +31,7 @@ def decode_scene_dir(scene_dir):
             # `time` can be: `13_53_00`, `13_53_00_ETC2`, `14_35_23_CB11_SIR18`, etc.
             time = ':'.join(time[0:3])
         else:
-            raise CDSRDecodeException(f'Invalid spplited time: `{scene_dir_second}`.')
+            raise CDSRDecoderException(f'Invalid spplited time: `{scene_dir_second}`.')
 
     elif scene_dir_first.startswith('CBERS2B') or scene_dir_first.startswith('LANDSAT'):
         # examples: CBERS2B_CCD_20070925.145654
@@ -42,20 +42,20 @@ def decode_scene_dir(scene_dir):
 
         if len(date) != 8:
             # example: a date should be something like this: '20070925'
-            raise CDSRDecodeException(f'Size of `{date}` date is not 8.')
+            raise CDSRDecoderException(f'Size of `{date}` date is not 8.')
 
         # I build the date string based on the old one (e.g. from '20070925' to '2007-09-25')
         date = f'{date[0:4]}-{date[4:6]}-{date[6:8]}'
 
         if len(time) != 6:
             # example: a time should be something like this: '145654'
-            raise CDSRDecodeException(f'Size of `{time}` time is not 6.')
+            raise CDSRDecoderException(f'Size of `{time}` time is not 6.')
 
         # I build the time string based on the old one (e.g. from '145654' to '14:56:54')
         time = f'{time[0:2]}:{time[2:4]}:{time[4:6]}'
 
     else:
-        raise CDSRDecodeException(f'Invalid scene directory: `{scene_dir}`.')
+        raise CDSRDecoderException(f'Invalid scene directory: `{scene_dir}`.')
 
     return satellite, sensor, date, time
 
@@ -72,7 +72,7 @@ def decode_path_row_dir(path_row_dir):
         # example: `151_B_141_5_0`
         path, _, row, *_ = splitted_path_row
     else:
-        raise CDSRDecodeException(f'`{path_row_dir}` path/row directory cannot be decoded.')
+        raise CDSRDecoderException(f'`{path_row_dir}` path/row directory cannot be decoded.')
 
     return path, row
 
@@ -85,7 +85,7 @@ def decode_geo_processing_dir(geo_processing_dir):
     if geo_processing in ('2', '2B', '3', '4'):
         return geo_processing
 
-    raise CDSRDecodeException(f'`{geo_processing_dir}` geo. processing '
+    raise CDSRDecoderException(f'`{geo_processing_dir}` geo. processing '
                                 'directory cannot be decoded.')
 
 
@@ -93,7 +93,7 @@ def decode_asset(asset):
     """Decodes a asset file, returning if this file is DN or SR."""
 
     if not asset.endswith('.tif') and not asset.endswith('.xml'):
-        raise CDSRDecodeException('Just TIFF and XML files can be decoded.')
+        raise CDSRDecoderException('Just TIFF and XML files can be decoded.')
 
     if 'GRID_SURFACE' in asset or 'EVI' in asset or 'NDVI' in asset:
         return 'SR'
@@ -119,7 +119,7 @@ def decode_path(path):
 
     # if this path is not level 6 or 7, then raise an exception
     if level not in (6, 7):
-        raise CDSRDecodeException(f'Invalid `{level}` level to path: `{path}`.')
+        raise CDSRDecoderException(f'Invalid `{level}` level to path: `{path}`.')
 
     # add the metadata based on the directory decode
     metadata = {
@@ -137,15 +137,3 @@ def decode_path(path):
     metadata['geo_processing'] = decode_geo_processing_dir(geo_processing_dir)
 
     return metadata
-
-
-# def generate_item_name_from_metadata(metadata):
-#     # AMAZONIA1_WFI_037016_20210321_L2_DN
-
-#     f"{metadata['satellite']}_"
-#     pass
-
-
-# def generate_collection_from_metadata(metadata):
-#     # AMAZONIA1_WFI_L2_DN
-#     pass
