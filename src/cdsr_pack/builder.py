7cec666d33e43f223c883a4fb3db3aa86da96f87
Rodrigo Mariano <rodmariano13@gmail.com>
2021-05-05 13:58:36 -0300

    feat: add function to build item

--- src/cdsr_pack/builder.py
+++ src/cdsr_pack/builder.py
@@ -25,8 +25,22 @@ def build_collection(metadata: dict) -> str:
            f"L{metadata['geo_processing']}_{metadata['radio_processing']}"
 
 
-# def generate_item_name_from_metadata(metadata):
-#     # AMAZONIA1_WFI_037016_20210321_L2_DN
+def build_item(metadata: dict) -> str:
+    """Builds item name based on metadata dict."""
 
-#     f"{metadata['satellite']}_"
-#     pass
+    # check metadata type
+    if not isinstance(metadata, dict):
+        raise CDSRBuilderException(f'Metadata must be a dict, not a `{type(metadata)}`.')
+
+    # mandatory keys
+    keys = ['satellite', 'sensor', 'path', 'row', 'date', 'geo_processing', 'radio_processing']
+
+    # get all keys that are missing inside metadata dict
+    missing_keys = [k for k in keys if k not in metadata]
+
+    if missing_keys:
+        raise CDSRBuilderException(f"Missing keys inside metadata: `{', '.join(missing_keys)}`.")
+
+    return f"{metadata['satellite']}_{metadata['sensor']}_" \
+           f"{metadata['path']}{metadata['row']}_{metadata['date'].replace('-', '')}_" \
+           f"L{metadata['geo_processing']}_{metadata['radio_processing']}"
