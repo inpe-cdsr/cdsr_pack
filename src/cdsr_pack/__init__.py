fb9c412005d7bf3ae7ab2d9c1ef9bcc62fb4bded
Rodrigo Mariano <rodmariano13@gmail.com>
2021-05-20 08:39:42 -0300

    chore: update package version to 0.0a0.dev1

--- src/cdsr_pack/__init__.py
+++ src/cdsr_pack/__init__.py
@@ -1,6 +1,6 @@
 """__init__.py"""
 
-__version__ = '0.0.dev0'
+__version__ = '0.0a0.dev1'
 
 from .builder import CDSRBuilderException, build_collection, build_item
 from .decoder import CDSRDecoderException, decode_path
